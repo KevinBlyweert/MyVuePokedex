33d6a69c515bd4eabbe2ab5c8a70fcb36bf004cc
Neithan et Eldalote Coemgen <kevin.blyweert@gmail.com>
2024-02-24 16:38:38 +0100

    changes to list display

--- src/components/type.vue
+++ src/components/type.vue
@@ -10,7 +10,7 @@ export default{
 #pokeType{
     width: 20px;height: 20px;border-radius: 50%;z-index: 20;will-change: width;transition: all .3s;color:rgba(255,255,255,0);font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
     &:hover{
-        width: 50px;border-radius: 5px;color:#fff;text-align: center;
+        width: 50px;border-radius: 5px;color:#fff;text-align: center;text-shadow: 0 0 3px #000;
     }
 }
 </style>
