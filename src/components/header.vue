ab4cbea91a0fd5c858d60347703c1153d0add64b
Neithan et Eldalote Coemgen <kevin.blyweert@gmail.com>
2024-03-06 21:17:15 +0100

    changes to css for pages nav

--- src/components/header.vue
+++ src/components/header.vue
@@ -9,6 +9,7 @@
 </script>
 <style scoped>
 #title{display: flex;flex-direction: row;gap: 15px;padding: 15px;align-items: center;}
-a{color:black;font-size: 2em;text-decoration: none;}
-img{width: 45px;height: auto;}
+a{color:black;font-size: 2em;text-decoration: none;font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;color: var(--bgcolor);}
+img{width: 45px;height: auto; &:hover{animation: rotate .5s linear infinite;}}
+@keyframes rotate{from{transform: rotate(0deg);}to{transform: rotate(360deg);}}
 </style>
